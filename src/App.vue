7726632fd31e9552cac6cded7f440a83d6c9a2f4
Yohann Ravino <atticot.yohann@gmail.com>
2020-08-04 09:16:34 +0200

    clean up starter kit Vue/Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,34 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <img alt="Vue logo" src="./assets/Logo-5HD.png">
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a href="/stories" class="nav-link">Stories</a>
-        </li>
-        <li class="nav-item">
-          <a href="/add" class="nav-link">Add</a>
-        </li>
-      </div>
-    </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-    <div class="container mt-3">
-      <router-view />
-    </div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a href="/stories" class="nav-link">Stories</a>
+              </li>
+              <li class="nav-item">
+                <a href="/add" class="nav-link">Add</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+            <router-view />
+      </div>
   </div>
 </template>
 
 <script>
-
+import "bootstrap/dist/css/bootstrap.min.css";
 export default {
   name: 'App',
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
